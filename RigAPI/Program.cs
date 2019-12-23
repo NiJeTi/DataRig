129c91ae60e2c651712f418104e95e52657bf0b4
NiJeTi <nijeti.inc@gmail.com>
2019-12-23 14:51:41 +0300

    Main class became static

--- RigAPI/Program.cs
+++ RigAPI/Program.cs
@@ -3,7 +3,7 @@ using Microsoft.Extensions.Hosting;
 
 namespace RigAPI
 {
-    public class Program
+    public static class Program
     {
         public static void Main(string[] args)
         {
