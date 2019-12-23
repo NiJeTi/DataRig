6a9a70df5312984bcbf65466cd4737789fd1ca4f
NiJeTi <nijeti.inc@gmail.com>
2019-12-23 19:32:59 +0300

    More wrappers

---
+++ RigAPI/Wrappers/Elastic.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+using Nest;
+
+namespace RigAPI.Wrappers
+{
+    public sealed class Elastic
+    {
+        public readonly ElasticClient client;
+
+        public Elastic (string connectionString, string index)
+        {
+            var uri = new Uri($"http://{connectionString}/");
+
+            var settings = new ConnectionSettings(uri)
+                .DefaultIndex(index)
+                .PrettyJson();
+
+            client = new ElasticClient(settings);
+        }
+    }
+}
---
+++ RigAPI/Wrappers/Neo4j.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+using Neo4j.Driver;
+
+namespace RigAPI.Wrappers
+{
+    public sealed class Neo4j : IDisposable
+    {
+        public readonly IDriver       driver;
+        public readonly IAsyncSession session;
+
+        public Neo4j (string connectionString, string username, string password)
+        {
+            var uri = new Uri($"bolt://{connectionString}/");
+
+            driver = GraphDatabase.Driver(uri, AuthTokens.Basic(username, password));
+
+            session = driver.AsyncSession();
+        }
+
+        public void Dispose()
+        {
+            session.CloseAsync();
+
+            if (driver == null)
+                return;
+
+            driver.CloseAsync();
+            driver.Dispose();
+        }
+    }
+}
