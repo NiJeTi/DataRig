657d27c122f214cdcbfc260af11a14c065b784b2
NiJeTi <nijeti.inc@gmail.com>
2019-12-23 14:53:15 +0300

    Finished authors registration system

--- RigAPI/Controllers/AuthorsController.cs
+++ RigAPI/Controllers/AuthorsController.cs
@@ -1,11 +1,11 @@
-﻿using System;
-using System.Threading.Tasks;
+﻿using System.Threading.Tasks;
 
 using Microsoft.AspNetCore.Mvc;
 
 using Npgsql;
 
-using RigAPI.Models.Authors;
+using RigAPI.Models;
+using RigAPI.Wrappers;
 
 namespace RigAPI.Controllers
 {
@@ -13,24 +13,60 @@ namespace RigAPI.Controllers
     [Route("authors")]
     public sealed class AuthorsController : ControllerBase
     {
-        private NpgsqlConnection connection;
+        private Postgres postgres;
 
-        public AuthorsController(NpgsqlConnection connection) => this.connection = connection;
+        public AuthorsController(Postgres postgres) => this.postgres = postgres;
 
         [HttpGet("{id}")]
-        [ProducesResponseType(typeof(string), 200)]
+        [ProducesResponseType(typeof(Author), 200)]
         [ProducesResponseType(404)]
         public async Task<IActionResult> GetAuthor(int id)
         {
-            throw new NotImplementedException();
+            Author author = default;
+
+            await using (var command = new NpgsqlCommand($"SELECT * FROM authors " +
+                                                         $"WHERE authors.id = {id}", postgres.connection))
+            {
+                await using var reader = await command.ExecuteReaderAsync();
+
+                if (await reader.ReadAsync())
+                {
+                    author = new Author
+                    {
+                        ID   = reader[0] as int?,
+                        Name = reader[1] as string,
+                        Bio  = reader[2] as string
+                    };
+                }
+
+                await reader.CloseAsync();
+            }
+
+            if (author is null)
+                return NotFound();
+
+            return Ok(author);
         }
 
         [HttpPost("register")]
-        [ProducesResponseType(200)]
-        [ProducesResponseType(409)]
-        public async Task<IActionResult> RegisterAuthor([FromBody] NewAuthor author)
+        [ProducesResponseType(typeof(int), 200)]
+        public async Task<IActionResult> RegisterAuthor([FromBody] Author author)
         {
-            throw new NotImplementedException();
+            int authorID;
+
+            await using (var command =
+                new NpgsqlCommand($"SELECT * FROM add_author('{author.Name}', '{author.Bio}')", postgres.connection))
+            {
+                await using var reader = await command.ExecuteReaderAsync();
+
+                await reader.ReadAsync();
+
+                authorID = (int) reader[0];
+
+                await reader.CloseAsync();
+            }
+
+            return Ok(authorID);
         }
     }
 }
