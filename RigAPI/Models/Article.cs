eb1d7c97f31787a02f3d3bd38ce8a718ffe1ca77
NiJeTi <nijeti.inc@gmail.com>
2019-12-23 19:34:38 +0300

    Changed type of Images array and added References array

--- RigAPI/Models/Article.cs
+++ RigAPI/Models/Article.cs
@@ -1,13 +1,12 @@
-﻿using MongoDB.Bson;
-
-namespace RigAPI.Models
+﻿namespace RigAPI.Models
 {
     public sealed class Article
     {
-        public int?       ID       { get; set; }
-        public int        AuthorID { get; set; }
-        public string     Text     { get; set; }
-        public ObjectId[] Images   { get; set; }
-        public string[]   Tags     { get; set; }
+        public int?     ID         { get; set; }
+        public int      AuthorID   { get; set; }
+        public string   Text       { get; set; }
+        public string[] Images     { get; set; }
+        public int[]    References { get; set; }
+        public string[] Tags       { get; set; }
     }
 }
