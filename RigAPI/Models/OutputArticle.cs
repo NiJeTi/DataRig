e3a4a72de10343e89a1ab34a300e0a2f8264f561
NiJeTi <nijeti.inc@gmail.com>
2020-01-10 19:01:25 +0300

    New model for references

--- RigAPI/Models/OutputArticle.cs
+++ RigAPI/Models/OutputArticle.cs
@@ -4,17 +4,17 @@ namespace RigAPI.Models
 {
     public sealed class OutputArticle
     {
-        public string              AuthorName { get; set; }
-        public string              Title      { get; set; }
-        public string              Text       { get; set; }
-        public List<byte[]>        Images     { get; }
-        public List<string>        References { get; }
-        public IEnumerable<string> Tags       { get; set; }
+        public string                AuthorName { get; set; }
+        public string                Title      { get; set; }
+        public string                Text       { get; set; }
+        public List<byte[]>          Images     { get; }
+        public List<OutputReference> References { get; }
+        public IEnumerable<string>   Tags       { get; set; }
 
         public OutputArticle ()
         {
             Images     = new List<byte[]>();
-            References = new List<string>();
+            References = new List<OutputReference>();
         }
     }
 }
---
+++ RigAPI/Models/OutputReference.cs
@@ -0,0 +1,8 @@
+﻿namespace RigAPI.Models
+{
+    public sealed class OutputReference
+    {
+        public int    ID    { get; set; }
+        public string Title { get; set; }
+    }
+}
